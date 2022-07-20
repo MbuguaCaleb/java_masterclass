f4e3e286641705f94c2a0ddde7d6178d60152795
MbuguaCaleb <mbuguacaleb30@gmail.com>
2022-07-20 12:41:31 +0300

    java-hello-world

---
+++ readme.md
@@ -0,0 +1,15 @@
+**Concepts**
+
+
+```aidl
+
+Package
+
+It is a collection of many classes.
+
+
+Classes in Java are organised or grouped
+in Packages.
+
+
+```
---
+++ src/com/codewithcaleb/Main.java
@@ -0,0 +1,17 @@
+//specifies what package the class belongs to
+package com.codewithcaleb;
+
+public class Main {
+
+    //public method-Means it is accessible from other parts of this programme
+    //static - The Main method in our programme should always be static
+    //void -means it will not return a value(Otherwise i would call the function to have it print
+    //We have one parameter for this function---called args and it is a String
+
+    public static void main(String[] args){
+
+        //comments
+        //Two slashes and they do not get executed
+        System.out.println("Hello World!");
+    }
+}
