7220e64ade7bdc817071fc7a0bbc64aec0cdddab
MbuguaCaleb <mbuguacaleb30@gmail.com>
2022-07-25 08:17:40 +0300

    user-input

--- src/com/codewithcaleb/Main.java
+++ src/com/codewithcaleb/Main.java
@@ -1,46 +1,23 @@
-
 package com.codewithcaleb;
+
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-       //The Math Class
-
-        //It has a number of useful methods
-
-        //When the same method is implemented twice we say it has been overloaded
-
-        //We use this method to round a floating point number to a whole number
-
-        //        int result = Math.round(1.1F);
-        //        System.out.println(result);
-        //
-        //        int resultCeil = (int) Math.ceil(1.1F);
-        //        System.out.println(resultCeil);
-
-
-        //        int result = Math.max(1,2);
-        //        System.out.println(result);
-
-        //        int result = Math.min(1,2);
-        //        System.out.println(result);
-
-        //Math Random returns a random number between zero and one
-
-        //Math random always returns a double
-        // double result = Math.random();
-        // System.out.println(result);
-
-
-        //Returning a Number Between 0 to 100
-
-        //Explicit casting is where i want to cast regardaless
-        //If i still want it to be an Integer i will explicitly cast it
-
-       //int result = (int) Math.round(Math.random() * 100);
-       //System.out.println(result);
-
-        int result = (int) (Math.random() * 100);
-
-        System.out.println(result);
+        //to read input from the console we use System.in
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name: ");
+        //i can get various types of inputs so i must specify
+        //There are other methods like nextFloat and nextDouble
+       // byte age = scanner.nextByte();
+        //we do not have next String but rather we have the Next Method
+        //The next method reads Only a single token
+        //String age = scanner.next()
+        //NextLine reads all the lines that the user has entered,no matter how many spaces or tabs
+        // that are there
+        String name = scanner.nextLine().trim();
+        System.out.println("You are " + name);
 
     }
 }
