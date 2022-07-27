f774da9cb07b3a7939d5f2adc3f503ffc7878066
MbuguaCaleb <mbuguacaleb30@gmail.com>
2022-07-27 09:17:33 +0300

    fizzbuzz

--- src/com/codewithcaleb/Main.java
+++ src/com/codewithcaleb/Main.java
@@ -1,15 +1,12 @@
 package com.codewithcaleb;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.print("Number:");
         //i create an instance of the scanner class
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-
         if((number % 5 == 0) && (number % 3)== 0)
             System.out.println("FizzBuzz");
         else if(number % 5 == 0)
@@ -18,5 +15,17 @@ public class Main {
             System.out.println("Buzz");
         else
             System.out.println(number);
+
+        //SOLUTION TWO
+        if(number % 5 == 0){
+            if(number % 3 == 0)
+                System.out.println("FizzBuzz");
+            else
+                System.out.println("Fizz");
+        }else if(number % 3 == 0 ){
+            System.out.println("Buzz");
+        }else{
+            System.out.println(number);
+        }
     }
 }
