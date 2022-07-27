3ffa866a771b2cb974fd66d282f2e0ba0fda56ce
MbuguaCaleb <mbuguacaleb30@gmail.com>
2022-07-27 08:19:01 +0300

    ternary-operator

--- src/com/codewithcaleb/Main.java
+++ src/com/codewithcaleb/Main.java
@@ -1,33 +1,7 @@
 package com.codewithcaleb;
 
 public class Main {
-    public static void main(String[] args){
-
-        //Method One
-        //        int temp = 32;
-        //        if(temp > 30){
-        //            System.out.println("It's a hot day");
-        //            System.out.println("Drink Water");
-        //        }else if(temp > 20 && temp <=30){
-        //            System.out.println("Beautiful Day");
-        //        }else{
-        //            System.out.println("Cold Day ");
-        //        }
-
-        //Method Two
-        //IF you are printing Only One clause you can do away with the braces
-
-        int temp = 32;
-        if(temp > 30) {
-            System.out.println("It's a hot day");
-            System.out.println("Drink Water");
-        }
-        else if(temp > 20)
-            System.out.println("Beautiful Day");
-        else
-            System.out.println("Cold Day ");
-
-
+    public static void main(String[] args) {
 
 
     }
