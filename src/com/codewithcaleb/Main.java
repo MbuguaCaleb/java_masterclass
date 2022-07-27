8eea2047d908f70c596604b4cf06938aad8d06cd
MbuguaCaleb <mbuguacaleb30@gmail.com>
2022-07-27 04:47:43 +0300

    logical-operators

--- src/com/codewithcaleb/Main.java
+++ src/com/codewithcaleb/Main.java
@@ -1,52 +1,32 @@
 package com.codewithcaleb;
 
-import java.text.NumberFormat;
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args){
 
-        //        System.out.print("Principal:");
-        //        Scanner scanner = new Scanner(System.in);
-        //        Double principalAmount = scanner.nextDouble();
-        //        System.out.println("Annual Interest Rate:");
-        //        Double annualInterestRate = scanner.nextDouble();
-        //        System.out.println("Period:");
-        //        Integer payments = scanner.nextInt();
-        //
-        //        //Interest formulae
-        //        Double monthlyInterestRate = (annualInterestRate/12)/100;
-        //
-        //        Double mortgage = principalAmount * (((monthlyInterestRate)*(Math.pow((1 +monthlyInterestRate),payments)))/((Math.pow((1 +monthlyInterestRate),payments))-1));
-        //
-        //        NumberFormat currencyFormat= NumberFormat.getCurrencyInstance();
-        //
-        //        System.out.println(currencyFormat.format(mortgage));
-        //        System.out.print(mortgage);
+        //Contol Flow
+        //Comparison Operators
 
-        //Solution Two
-        final byte MONTHS_IN_YEAR =12;
-        final byte PERCENT = 100;
+        //We use comparison Operators to Compare primitive Values
+        int x =1;
+        int y =2;
 
-        Scanner scanner = new Scanner(System.in);
+        //One equals sign stands for assignment
+        //Two equals signs represent the /equality/comparion operator
+        System.out.println(x==y);
+        //Inequality Operator
+        System.out.println(x!=y);
+        System.out.println(x<=y);
+        System.out.println(x>=y);
 
-        System.out.print("Principal: ");
-        Integer principal = scanner.nextInt();
 
-        System.out.print("Annual Interest Rate: ");
-        Float annualInterest = scanner.nextFloat();
-        Float monthlyInterest = annualInterest /PERCENT/MONTHS_IN_YEAR;
+        //Logical Operators
+        //Condotional Statements
+        //Loops
 
-        System.out.println("Period (Years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
 
-        double mortgage = principal*(monthlyInterest * Math.pow(1+ monthlyInterest,numberOfPayments))/
-                (Math.pow(1+monthlyInterest,numberOfPayments) -1 );
+        //Conditional Statements
 
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage:" + mortgageFormatted);
+
 
     }
 }
