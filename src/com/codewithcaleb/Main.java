4b8cdeeba58d7e694eaa27c36df8ad91f0edd5ee
MbuguaCaleb <mbuguacaleb30@gmail.com>
2022-07-27 08:25:24 +0300

    switch-statements

--- src/com/codewithcaleb/Main.java
+++ src/com/codewithcaleb/Main.java
@@ -3,6 +3,31 @@ package com.codewithcaleb;
 public class Main {
     public static void main(String[] args) {
 
+      //Switch Statements
+        //
+        String role = "admin";
+        switch (role){
+            case "admin":
+                System.out.println("You are an Admin");
+                break;
+            case "moderator":
+                System.out.println("You are a Moderator");
+                break;
+            default:
+                System.out.println("You are a guest");
+
+        }
+
+//        if(role == "admin")
+//            System.out.println("You are an Admin");
+//        else if (role == "moderator")
+//            System.out.println("You are a moderator");
+//        else
+//            System.out.println("You are a guest");
+
+
+        //Switch Statement
+
 
     }
 }
