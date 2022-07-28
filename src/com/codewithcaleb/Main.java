a97a2649564383c8fb738e5c804e911114c89ed5
MbuguaCaleb <mbuguacaleb30@gmail.com>
2022-07-28 08:17:39 +0300

    improving-our-mortgage-calculator

--- src/com/codewithcaleb/Main.java
+++ src/com/codewithcaleb/Main.java
@@ -4,6 +4,10 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
+    //If i have common variables being used by methods of a Class, i can define them in the class scope
+    final static byte MONTHS_IN_YEAR =12;
+    final static byte PERCENT = 100;
+
     public static void main(String[] args) {
         //Refactoring
         //Changing the structure of our code without changing its behavior
@@ -13,14 +17,23 @@ public class Main {
         float annualInterest = (float) readNumber("Annual Interest Rate: ",1,30);
         byte years =(byte) readNumber("Period (Years): ",1,30);
 
-        //calling the calculate Mortgage Method
+        //calling to calculate Mortgage Method
         double mortgage = calculateMortgage(principal,annualInterest,years);
-
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("---------");
         System.out.println("Mortgage:" + mortgageFormatted);
 
-    }
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
 
+        for (short month=1; month <=years * MONTHS_IN_YEAR; month ++){
+            double balance = calculateBalance(principal,annualInterest,years,month);
+            System.out.println( NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
     public static double readNumber(String prompt,double min, double max){
         Scanner scanner = new Scanner(System.in);
         double value;
@@ -33,15 +46,23 @@ public class Main {
         }
         return  value;
     }
+    public static double calculateBalance(int principal,
+                                          float annualInterest,
+                                          byte years,
+                                          short numberOfPaymentsMade){
 
+        short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+        float monthlyInterest = annualInterest /PERCENT/MONTHS_IN_YEAR;
+        double balance = principal *(Math.pow(1+ monthlyInterest,numberOfPayments) - Math.pow(1+ monthlyInterest,numberOfPaymentsMade))
+                                   /(Math.pow(1+ monthlyInterest,numberOfPayments) -1);
+        return  balance;
+
+    }
     public static double calculateMortgage (int principal,
                                             float annualInterest,
                                             byte years){
-
-        //Always remember about scope, whenever you declare variables inside of a function,
+        //Always remember about scope, whenever you declare variables inside a function,
         //They Only belong to that particular scope
-        final byte MONTHS_IN_YEAR =12;
-        final byte PERCENT = 100;
         short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
         float monthlyInterest = annualInterest /PERCENT/MONTHS_IN_YEAR;
         double mortgage = principal*(monthlyInterest * Math.pow(1+ monthlyInterest,numberOfPayments))/
