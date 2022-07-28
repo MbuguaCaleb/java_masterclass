9679e99a96f6a4e77c636eac0e01ddf15c459a22
MbuguaCaleb <mbuguacaleb30@gmail.com>
2022-07-28 15:51:46 +0300

    debugging-applications

--- src/com/codewithcaleb/Main.java
+++ src/com/codewithcaleb/Main.java
@@ -2,33 +2,17 @@ package com.codewithcaleb;
 
 
 public class Main {
-
+    //Everything is instantiated from the main programme in Java
     public static void main(String[] args) {
+        System.out.println("Start");
+        printNumbers(4);
+        System.out.println("Finish");
 
-        //Java Rules
-        //1 .We Must Always specify the datatypes of our variables
-        //Other Programming Languages are More linient but Java is Not
-        String name ="CALEB";
+    }
 
-        //2.We must terminate our Statements with semi Colons unless we are defining a code block
-        int Age = 12;
-
-        //3.When we are calling methods we must always include a paranthesis.
-        System.out.println();
-
-
-        //4 Strings in Java are represented by Double Quotes
-        //Single quotes are reserved for Characters unlike in other Languages like Python
-
-        //5 Java is Case sensitive so we Must always ensure that our variables are named correctly
-
-        //6.We cannot name our variables using keywords/ such as class
-
-        //7 We cannot use a single equals sign to compare values
-        //There is a big difference between one equals sign and two equals sign in Java.
-
-
-
-
+    public static void printNumbers(int limit){
+        for (int i=0; i< limit; i ++){
+            System.out.println(i);
+        }
     }
 }
